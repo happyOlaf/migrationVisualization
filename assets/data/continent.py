5899ebcc681a441547da254332f5b76b5d5093ad
cjs123456 <386509698@qq.com>
2020-06-13 23:14:33 +0800

    done

--- assets/data/continent.py
+++ assets/data/continent.py
@@ -14,16 +14,16 @@ def continent():
     flag = [0]*232
     error = 0
     success = 0
-    x = pan.read_excel('各大洲国家19版.xlsx' ) #encoding="gbk"
-    oricountry = pan.read_excel('各国每年移民数量处理/country_count/19.xlsx', encoding="gbk") 
-    txt = open("./continent.txt",mode='w',encoding='UTF-8')
+    x = pan.read_excel('国家收入分组.xlsx' ) #encoding="gbk"
+    oricountry = pan.read_excel('各国每年移民数量处理/country_count/90.xlsx', encoding="gbk") 
+    txt = open("./income.txt",mode='w',encoding='UTF-8')
     # print(oricountry.iloc[2,3])
     # print(x.iloc[57,3])
     for i in range(oricountry.index.size):
       for p in range(x.index.size):
-        for q in range(6): # 6大洲
+        for q in range(3): # 6大洲
           if(oricountry.iloc[i,2] == x.iloc[p,q]):
-            continentoutcount[q] = continentoutcount[q] + oricountry.iloc[i,5]
+            continentoutcount[q] = continentoutcount[q] + oricountry.iloc[i,3]
             #continentincount[q] = continentincount[q] + oricountry.iloc[i,1]
             success = success + 1
             flag[i] = 1
